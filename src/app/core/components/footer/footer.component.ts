5cfbc68db79e283e8dd43d2870f545a62ce6a40e
Fane Yaya <y.fane75@hotmail.fr>
2026-01-08 00:24:15 +0100

    Ajout du footer responsive mais pas fixe

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+/**
+ * @title Toolbar with just text
+ */
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
+
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
