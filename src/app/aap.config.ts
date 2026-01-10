b4c024b9a2a069d1dd07e3ca95899182da7d04b6
Fane Yaya <y.fane75@hotmail.fr>
2026-01-11 00:36:26 +0100

    c carre

--- src/app/aap.config.ts
+++ src/app/aap.config.ts
@@ -1,10 +1,38 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { RouterModule } from '@angular/router';
+import { routes } from './app-routing.module';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { MatNativeDateModule } from '@angular/material/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/lang/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
-    importProvidersFrom(MatNativeDateModule)
+    importProvidersFrom(
+      HttpClientModule,
+      MatNativeDateModule,
+
+      // ✅ ROUTER (OBLIGATOIRE)
+      RouterModule.forRoot(routes),
+
+      // ✅ TRANSLATE
+      TranslateModule.forRoot({
+        defaultLanguage: 'fr',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    )
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { routes } from './app/app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './app/shared/shared.module';
-import { CoreModule } from './app/core/core.module';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { appConfig } from './app/aap.config';
+// import { appConfig } from './app/app.config';
 
-
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/lang/', '.json');
-}
-
-bootstrapApplication(AppComponent, appConfig){
-  providers: [
-    importProvidersFrom(
-      RouterModule.forRoot(routes),
-      BrowserAnimationsModule,
-      HttpClientModule,
-      SharedModule,
-      CoreModule,
-      TranslateModule.forRoot({
-        defaultLanguage: 'fr',
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        }
-      })
-    )
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
