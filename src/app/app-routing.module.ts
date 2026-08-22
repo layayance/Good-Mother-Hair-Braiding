d19de599747304e322c343f929da22eb91e8f5e6
Fane Yaya <y.fane75@hotmail.fr>
2026-08-22 18:22:00 +0200

    Add local SEO for Trenton hair salon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 import {GalerieComponent} from "./pages/galerie/dashboard.component";
 
 export const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m =>m.ContactModule) },
-  { path: 'services', loadChildren: () => import('./pages/services/services-propose.module').then(m =>m.ServicesProposeModule) },
-  { path: 'dashboard', component: GalerieComponent },
+  {
+    path: 'home',
+    title: 'African Hair Braiding Salon in Trenton, NJ | Good Mother',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'contact',
+    title: 'Book a Hair Braiding Appointment in Trenton, NJ | Good Mother',
+    loadChildren: () => import('./pages/contact/contact.module').then(m =>m.ContactModule)
+  },
+  {
+    path: 'services',
+    title: 'Braids, Cornrows & Weaves in Trenton, NJ | Good Mother',
+    loadChildren: () => import('./pages/services/services-propose.module').then(m =>m.ServicesProposeModule)
+  },
+  {
+    path: 'dashboard',
+    title: 'African Braids & Hairstyle Gallery | Good Mother Trenton',
+    component: GalerieComponent
+  },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
